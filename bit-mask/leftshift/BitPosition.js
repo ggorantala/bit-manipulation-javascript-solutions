b85839f84d5bd0210a9f030c0bc8ea5d39546e91
Gopi Gorantala <gopishiva001@outlook.com>
2022-09-06 14:35:18 +0530

    first set bit position solution added

---
+++ bit-mask/leftshift/BitPosition.js
@@ -0,0 +1,26 @@
+// Time: O(1), always constant, as we are dealing with bit representation of decimals or ASCII.
+// They are represented in either 32/64
+// Space: O(1), no extra memory allocated
+const FirstSetBitPosition = (number) => {
+  function helper(n) {
+    if (n === 0) {
+      return 0;
+    }
+
+    let k = 1;
+
+    while (true) {
+      if ((n & (1 << (k - 1))) === 0) {
+        k++;
+      } else {
+        return k;
+      }
+    }
+  }
+
+  return helper(number);
+}
+
+console.log(`First set-bit position for number: 18 is -> ${FirstSetBitPosition(18)}`);
+console.log(`First set-bit position for number: 5 is -> ${FirstSetBitPosition(5)}`);
+console.log(`First set-bit position for number: 32 is -> ${FirstSetBitPosition(32)}`);
